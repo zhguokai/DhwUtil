0fc8f8b864716896b79eb6e93f25361f1e90dc18
zhangxixi <zhangqiang@kua-jie.com>
2016-06-25 11:59:23 +0800

    init

---
+++ src/dhwutil/convertutil.go
@@ -0,0 +1,64 @@
+package dhwutil
+
+import (
+	"strconv"
+	"errors"
+	"fmt"
+)
+
+func ToInt64(v interface{}) (intValue int64, err error) {
+	switch v.(type) {
+	case string:
+		if iV, err := strconv.Atoi(v.(string)); err == nil {
+			return int64(iV), nil
+		} else {
+			return 0, err
+		}
+
+	case int:
+		return int64(v.(int)), nil
+	case int8:
+		return int64(v.(int8)), nil
+	case int16:
+		return int64(v.(int16)), nil
+	case int32:
+		return int64(v.(int32)), nil
+	case int64:
+		return int64(v.(int64)), nil
+	case float32:
+		return int64(v.(float32)), nil
+	case float64:
+		return int64(v.(float64)), nil
+	default:
+		return 0, errors.New("无效的类型")
+	}
+
+}
+
+//转字符串
+func ToString(v interface{}) (res string) {
+	switch v.(type) {
+	case string:
+		return v.(string)
+	case int:
+		return fmt.Sprintf("%d", v)
+	case int8:
+		return fmt.Sprintf("%d", v)
+	case int16:
+		return fmt.Sprintf("%d", v)
+	case int32:
+		return fmt.Sprintf("%d", v)
+	case int64:
+		return fmt.Sprintf("%d", v)
+	case uint:
+		return fmt.Sprintf("%d", v)
+	case float32:
+		return fmt.Sprintf("%.2f", v)
+	case float64:
+		return fmt.Sprintf("%2f", v)
+
+	default:
+		return "error"
+	}
+
+}
---
+++ src/dhwutil/smsutil.go
@@ -0,0 +1,48 @@
+package dhwutil
+
+import (
+	"errors"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"encoding/xml"
+	"log"
+)
+
+
+//发送自定义短信
+func SendYmMsgToMobile(mobile, signName, msg, smsCdKey, smsPass string) error {
+	smsGateWayUrl := "http://sdk4rptws.eucp.b2m.cn:8080/sdkproxy/sendsms.action"
+	if "" == mobile {
+		return errors.New("手机号不能为空！")
+	}
+	v := url.Values{}
+	v.Set("cdkey", smsCdKey)
+	v.Set("password", smsPass)
+	v.Set("phone", mobile)
+	v.Set("message", "【" + signName + "】" + msg)
+	if resp, err := http.PostForm(smsGateWayUrl, v); err == nil {
+		defer resp.Body.Close()
+		data, _ := ioutil.ReadAll(resp.Body)
+		//xml.Unmarshal(data,&)
+		var resp YmSmsResp
+		err := xml.Unmarshal(data, &resp)
+		if err != nil {
+			log.Println(err.Error())
+			return err
+		}
+		if resp.Error == "0" {
+			return nil
+		}
+		return errors.New(resp.Message)
+	} else {
+		defer resp.Body.Close()
+		return err
+	}
+}
+
+type YmSmsResp struct {
+	XMLName xml.Name `xml:"response"`
+	Error   string   `xml:"error"`
+	Message string   `xml:"message"`
+}
